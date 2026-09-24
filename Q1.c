b7a23f5d2639405622bca4635bf162e6b687b65c
Sukrity06 <sukrity26csu173@ncuindia.edu>
2026-09-24 22:15:53 +0530

    Q1.c

---
+++ Q1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int studentID = 101;
+    int age = 18;
+    float percentage = 85.50;
+    char grade = 'A';
+
+    printf("Student ID: %d\n", studentID);
+    printf("Age: %d\n", age);
+    printf("Percentage: %.2f\n", percentage);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
