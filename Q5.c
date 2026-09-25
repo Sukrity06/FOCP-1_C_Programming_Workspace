b0dba0c2b50c6991643f71ea67564cf6207bc7fc
Sukrity06 <sukrity26csu173@ncuindia.edu>
2026-09-25 12:50:16 +0530

    Implement number swapping in Q5.c

---
+++ Q5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Before swapping: %d %d\n", a, b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping: %d %d\n", a, b);
+
+    return 0;
+}
