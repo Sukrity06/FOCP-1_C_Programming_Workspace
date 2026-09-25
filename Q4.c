e30e11313ac1965edb5781c4556f868ca605d907
Sukrity06 <sukrity26csu173@ncuindia.edu>
2026-09-25 12:44:50 +0530

    Q4.c

---
+++ Q4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    float average;
+
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    average = (a + b + c) / 3.0;
+
+    printf("Average = %.2f\n", average);
+
+    return 0;
+}
